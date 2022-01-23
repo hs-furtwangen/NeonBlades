9c37a7cdd5d0099cf07e9cfd0605367a6ab722d9
Monika <monika.galkewitsch@hs-furtwangen.de>
2022-01-23 03:16:26 +0100

    Fire Effect, Slicing Test

---
+++ Assets/Script/AddParticleSystem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddParticleSystem : MonoBehaviour
+{
+    public void testCallback() {
+        Debug.Log("A slice has occured");
+    }
+}
